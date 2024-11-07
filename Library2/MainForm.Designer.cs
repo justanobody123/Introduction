1214bd0478d1b0c0582537990f9aa37178cb14df
Кристина Волосова <justanobody017527@gmail.com>
2024-11-07 21:40:51 +0300

    HW DONE

---
+++ Library2/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Library2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxQuery = new System.Windows.Forms.TextBox();
+			this.buttonExecute = new System.Windows.Forms.Button();
+			this.dataGridView = new System.Windows.Forms.DataGridView();
+			this.comboBoxTables = new System.Windows.Forms.ComboBox();
+			this.buttonComboBoxTable = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBoxQuery
+			// 
+			this.textBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxQuery.Location = new System.Drawing.Point(13, 13);
+			this.textBoxQuery.Name = "textBoxQuery";
+			this.textBoxQuery.Size = new System.Drawing.Size(412, 22);
+			this.textBoxQuery.TabIndex = 0;
+			// 
+			// buttonExecute
+			// 
+			this.buttonExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonExecute.Location = new System.Drawing.Point(431, 12);
+			this.buttonExecute.Name = "buttonExecute";
+			this.buttonExecute.Size = new System.Drawing.Size(75, 24);
+			this.buttonExecute.TabIndex = 1;
+			this.buttonExecute.Text = "Execute";
+			this.buttonExecute.UseVisualStyleBackColor = true;
+			this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click);
+			// 
+			// dataGridView
+			// 
+			this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView.Location = new System.Drawing.Point(13, 71);
+			this.dataGridView.Name = "dataGridView";
+			this.dataGridView.RowHeadersWidth = 51;
+			this.dataGridView.RowTemplate.Height = 24;
+			this.dataGridView.Size = new System.Drawing.Size(493, 185);
+			this.dataGridView.TabIndex = 2;
+			// 
+			// comboBoxTables
+			// 
+			this.comboBoxTables.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBoxTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxTables.FormattingEnabled = true;
+			this.comboBoxTables.Location = new System.Drawing.Point(13, 41);
+			this.comboBoxTables.Name = "comboBoxTables";
+			this.comboBoxTables.Size = new System.Drawing.Size(412, 24);
+			this.comboBoxTables.TabIndex = 3;
+			// 
+			// buttonComboBoxTable
+			// 
+			this.buttonComboBoxTable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonComboBoxTable.Location = new System.Drawing.Point(431, 41);
+			this.buttonComboBoxTable.Name = "buttonComboBoxTable";
+			this.buttonComboBoxTable.Size = new System.Drawing.Size(75, 24);
+			this.buttonComboBoxTable.TabIndex = 4;
+			this.buttonComboBoxTable.Text = "Select";
+			this.buttonComboBoxTable.UseVisualStyleBackColor = true;
+			this.buttonComboBoxTable.Click += new System.EventHandler(this.buttonComboBoxTable_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(518, 268);
+			this.Controls.Add(this.buttonComboBoxTable);
+			this.Controls.Add(this.comboBoxTables);
+			this.Controls.Add(this.dataGridView);
+			this.Controls.Add(this.buttonExecute);
+			this.Controls.Add(this.textBoxQuery);
+			this.Name = "MainForm";
+			this.Text = "Library2";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxQuery;
+		private System.Windows.Forms.Button buttonExecute;
+		private System.Windows.Forms.DataGridView dataGridView;
+		private System.Windows.Forms.ComboBox comboBoxTables;
+		private System.Windows.Forms.Button buttonComboBoxTable;
+	}
+}
+
---
+++ Library2/MainForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Runtime.InteropServices;
+
+namespace Library2
+{
+	public partial class MainForm : Form
+	{
+		string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString_2_Library_PD_311"].ConnectionString;
+		SqlConnection connection;
+		public MainForm()
+		{
+			connection = new SqlConnection(connectionString);
+#if DEBUG
+			AllocConsole(); 
+#endif
+			InitializeComponent();
+			textBoxQuery.Text = "SELECT * FROM Authors";
+			InitializeComboBoxTables();
+		}
+		void InitializeComboBoxTables()
+		{
+			connection.Open();
+			DataTable tables = connection.GetSchema("Tables");
+#if DEBUG
+			Console.WriteLine("tables.Rows.Count = " + tables.Rows.Count); 
+#endif
+			foreach (DataRow row in tables.Rows)
+			{
+				comboBoxTables.Items.Add(row[2].ToString());
+#if DEBUG
+				Console.WriteLine("table name - " + row[2].ToString()); 
+#endif
+			}
+			connection.Close();
+		}
+		private void buttonExecute_Click(object sender, EventArgs e)
+		{
+			string cmd = textBoxQuery.Text;
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			DataTable table = new DataTable();
+			SqlDataReader reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+			{
+				table.Columns.Add(reader.GetName(i));
+			}
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					row[i] = reader[i];
+				}
+				table.Rows.Add(row);
+			}
+			reader.Close();
+			connection.Close();
+			dataGridView.DataSource = table;
+		}
+
+		///////////////////////////////////////////////
+		[DllImport("kernel32.dll")]
+		static extern bool AllocConsole();
+
+		private void buttonComboBoxTable_Click(object sender, EventArgs e)
+		{
+			if (comboBoxTables.SelectedIndex != -1)
+			{
+				string cmd = $"SELECT * FROM [{comboBoxTables.SelectedItem.ToString()}]";
+#if DEBUG
+				Console.WriteLine(cmd);
+#endif
+				SqlCommand command = new SqlCommand(cmd, connection);
+				connection.Open();
+				DataTable table = new DataTable();
+				SqlDataReader reader = command.ExecuteReader();
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					table.Columns.Add(reader.GetName(i));
+				}
+				while (reader.Read())
+				{
+					DataRow row = table.NewRow();
+					for (int i = 0; i < reader.FieldCount; i++)
+					{
+						row[i] = reader[i];
+					}
+					table.Rows.Add(row);
+				}
+				reader.Close();
+				connection.Close();
+				dataGridView.DataSource = table;
+			}
+		}
+	}
+}
